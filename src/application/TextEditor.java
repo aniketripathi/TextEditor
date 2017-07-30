9d471bc80271632ef53fd4f7f8da793a886023b5
Aniket <aniketkumartripathi@gmail.com>
2017-07-30 12:56:43 +0530

    Add/Remove files

--- src/application/TextEditor.java
+++ src/application/TextEditor.java
@@ -1,5 +1,4 @@
 package application;
-	
 
 import frontend.SceneCreator;
 import javafx.application.Application;
@@ -8,33 +7,37 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
+
 public class TextEditor extends Application {
 	
 	public static final String TITLE = "Text Editor";
 	
 	
+	
 	@Override
 	public void start(Stage primaryStage) {
-	
-	primaryStage.setTitle(TITLE);
-	primaryStage.setIconified(false);
-	primaryStage.setResizable(true);
-	primaryStage.initStyle(StageStyle.DECORATED);
-	
-	
-	primaryStage.setMinHeight(100);
-	primaryStage.setMinWidth(200);
-	SceneCreator sceneCreator = new SceneCreator(primaryStage);
-	
-	Scene primaryScene = sceneCreator.getScene();
-
-	primaryStage.setScene(primaryScene);
-	primaryStage.setMaximized(true);
-	primaryStage.show();
-	
+		
+		primaryStage.setTitle(TITLE);
+		primaryStage.setIconified(false);
+		primaryStage.setResizable(true);
+		primaryStage.initStyle(StageStyle.DECORATED);
+		
+		primaryStage.setMinHeight(100);
+		primaryStage.setMinWidth(200);
+		SceneCreator sceneCreator = new SceneCreator(primaryStage);
+		
+		Scene primaryScene = sceneCreator.getScene();
+		
+		primaryStage.setScene(primaryScene);
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+		
 	}
 	
+	
+	
 	public static void main(String[] args) {
+		
 		launch(args);
 	}
 }
